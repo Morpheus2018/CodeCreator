6e3a843c857f6854eaaedb0cb53ba8709397f173
plinux <p>
2020-10-05 09:09:48 +0200

    python

---
+++ Qt Designer/CodeCreator_Qt.v1.0.0.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+import sys
+import PyQt5.uic as uic
+import PyQt5.QtWidgets as widgets
+
+app = widgets.QApplication(sys.argv)
+w = uic.loadUi("qt5designer.ui")
+
+
+def clickmethod():
+    output = f'<li><a href="{w.lineEditLINK.text()}" target="_blank"><img data-src="{w.lineEditBILD.text()}" class="lazyload" loading="lazy" title="{w.lineEditTITLE.text()}" border="2"/>{w.lineEditNAME.text()}</a></li>\n '
+    with open(FILENAME, "a", encoding='UTF-8') as output_file:
+        output_file.write(output)
+    print(f'{output}'), (w.lineEditLINK.clear()), (w.lineEditBILD.clear()), (w.lineEditTITLE.clear()), (w.lineEditNAME.clear())
+
+
+FILENAME = "output.txt"
+
+w.pushButtonSave.clicked.connect(clickmethod)
+
+w.show()
+sys.exit(app.exec_())
