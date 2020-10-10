8923bea7e7387c5733ca8a46184bc06f180d25e6
plinux <p>
2020-10-10 13:58:32 +0200

    Update Qt.v2.0.0

--- Qt Designer/CodeCreator_Qt.v1.0.0.py
+++ Qt Designer/CodeCreator_Qt.v1.0.0.py
@@ -19,11 +19,45 @@ def clickmethod():
     w.lineEditTITLE.clear()
     w.lineEditNAME.clear()
 
+def onClicked(self):
+    if w.radioButton_1.isChecked():
+        file = open("output.txt", "a")
+        file.write("Code für :" + w.radioButton_1.text() + "\n")
+        file.close()
+        w.groupBoxCode.setTitle("Code für " + w.radioButton_1.text())
+        print("Code für :" + w.radioButton_1.text() + "\n")
+
+def onClicked1(self):
+    if w.radioButton_2.isChecked():
+        file = open("output.txt", "a")
+        file.write("Code für :" + w.radioButton_2.text() + "\n")
+        file.close()
+        w.groupBoxCode.setTitle("Code für " + w.radioButton_1.text())
+        print("Code für :" + w.radioButton_2.text() + "\n")
+
+def onClicked2(self):
+    if w.radioButton_3.isChecked():
+        file = open("output.txt", "a")
+        file.write("Code für :" + w.radioButton_3.text() + "\n")
+        file.close()
+        w.groupBoxCode.setTitle("Code für " + w.radioButton_3.text())
+        print("Code für :" + w.radioButton_3.text() + "\n")
+
+def onClicked3(self):
+    if w.radioButton_4.isChecked():
+        file = open("output.txt", "a")
+        file.write("Code für :" + w.radioButton_4.text() + "\n")
+        file.close()
+        w.groupBoxCode.setTitle("Code für " + w.radioButton_4.text())
+        print("Code für :" + w.radioButton_4.text() + "\n")
 
 FILENAME = "output.txt"
 
 w.pushButtonSave.clicked.connect(clickmethod)
-#w.radioButton_1.clicked.toggled.connect(onClicked)
+w.radioButton_1.toggled.connect(onClicked)
+w.radioButton_2.toggled.connect(onClicked1)
+w.radioButton_3.toggled.connect(onClicked2)
+w.radioButton_4.toggled.connect(onClicked3)
 
 w.show()
 sys.exit(app.exec_())
