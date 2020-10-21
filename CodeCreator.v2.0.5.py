e4145c8e7afcf25c483bcdca57f1b71ff5967b6e
plinux <p>
2020-10-21 18:13:05 +0200

    Update v2.0.5

--- CodeCreator.v2.0.5.py
+++ CodeCreator.v2.0.5.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
+# Form implementation generated from reading ui file 'qt5designer.v2.0.5.ui'
+#
 # Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
 
-
-from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+import subprocess
+#import os #Für Windows
+from PyQt5 import QtCore, QtWidgets
 
 
 class Ui_dialog(object):
@@ -36,30 +42,6 @@ class Ui_dialog(object):
         self.groupBoxCode.setObjectName("groupBoxCode")
         self.gridLayout = QtWidgets.QGridLayout(self.groupBoxCode)
         self.gridLayout.setObjectName("gridLayout")
-        self.labelLINK = QtWidgets.QLabel(self.groupBoxCode)
-        self.labelLINK.setObjectName("labelLINK")
-        self.gridLayout.addWidget(self.labelLINK, 1, 0, 1, 1)
-        self.labelTITLE = QtWidgets.QLabel(self.groupBoxCode)
-        self.labelTITLE.setObjectName("labelTITLE")
-        self.gridLayout.addWidget(self.labelTITLE, 3, 0, 1, 1)
-        self.labelNAME = QtWidgets.QLabel(self.groupBoxCode)
-        self.labelNAME.setObjectName("labelNAME")
-        self.gridLayout.addWidget(self.labelNAME, 4, 0, 1, 1)
-        self.lineEditNAME = QtWidgets.QLineEdit(self.groupBoxCode)
-        self.lineEditNAME.setObjectName("lineEditNAME")
-        self.gridLayout.addWidget(self.lineEditNAME, 4, 1, 1, 1)
-        self.lineEditBILD = QtWidgets.QLineEdit(self.groupBoxCode)
-        self.lineEditBILD.setObjectName("lineEditBILD")
-        self.gridLayout.addWidget(self.lineEditBILD, 2, 1, 1, 1)
-        self.lineEditLINK = QtWidgets.QLineEdit(self.groupBoxCode)
-        self.lineEditLINK.setObjectName("lineEditLINK")
-        self.gridLayout.addWidget(self.lineEditLINK, 1, 1, 1, 1)
-        self.labelBILD = QtWidgets.QLabel(self.groupBoxCode)
-        self.labelBILD.setObjectName("labelBILD")
-        self.gridLayout.addWidget(self.labelBILD, 2, 0, 1, 1)
-        self.lineEditTITLE = QtWidgets.QLineEdit(self.groupBoxCode)
-        self.lineEditTITLE.setObjectName("lineEditTITLE")
-        self.gridLayout.addWidget(self.lineEditTITLE, 3, 1, 1, 1)
         self.splitter = QtWidgets.QSplitter(self.groupBoxCode)
         self.splitter.setOrientation(QtCore.Qt.Horizontal)
         self.splitter.setObjectName("splitter")
@@ -68,13 +50,40 @@ class Ui_dialog(object):
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.layoutWidget)
         self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.pushButtonSave = QtWidgets.QPushButton(self.layoutWidget)
-        self.pushButtonSave.setObjectName("pushButtonSave")
-        self.horizontalLayout_2.addWidget(self.pushButtonSave)
-        self.pushButtonEde = QtWidgets.QPushButton(self.layoutWidget)
-        self.pushButtonEde.setObjectName("pushButtonEde")
-        self.horizontalLayout_2.addWidget(self.pushButtonEde)
-        self.gridLayout.addWidget(self.splitter, 6, 1, 1, 1)
+        self.pushButtonNext = QtWidgets.QPushButton(self.layoutWidget)
+        self.pushButtonNext.setObjectName("pushButtonNext")
+        self.horizontalLayout_2.addWidget(self.pushButtonNext)
+        self.pushButtonOpen = QtWidgets.QPushButton(self.layoutWidget)
+        self.pushButtonOpen.setObjectName("pushButtonOpen")
+        self.horizontalLayout_2.addWidget(self.pushButtonOpen)
+        self.pushButtonExit = QtWidgets.QPushButton(self.layoutWidget)
+        self.pushButtonExit.setObjectName("pushButtonExit")
+        self.horizontalLayout_2.addWidget(self.pushButtonExit)
+        self.gridLayout.addWidget(self.splitter, 7, 1, 1, 1)
+        self.lineEditBILD = QtWidgets.QLineEdit(self.groupBoxCode)
+        self.lineEditBILD.setObjectName("lineEditBILD")
+        self.gridLayout.addWidget(self.lineEditBILD, 2, 1, 1, 1)
+        self.labelTITLE = QtWidgets.QLabel(self.groupBoxCode)
+        self.labelTITLE.setObjectName("labelTITLE")
+        self.gridLayout.addWidget(self.labelTITLE, 3, 0, 1, 1)
+        self.labelBILD = QtWidgets.QLabel(self.groupBoxCode)
+        self.labelBILD.setObjectName("labelBILD")
+        self.gridLayout.addWidget(self.labelBILD, 2, 0, 1, 1)
+        self.lineEditNAME = QtWidgets.QLineEdit(self.groupBoxCode)
+        self.lineEditNAME.setObjectName("lineEditNAME")
+        self.gridLayout.addWidget(self.lineEditNAME, 4, 1, 1, 1)
+        self.lineEditLINK = QtWidgets.QLineEdit(self.groupBoxCode)
+        self.lineEditLINK.setObjectName("lineEditLINK")
+        self.gridLayout.addWidget(self.lineEditLINK, 1, 1, 1, 1)
+        self.labelLINK = QtWidgets.QLabel(self.groupBoxCode)
+        self.labelLINK.setObjectName("labelLINK")
+        self.gridLayout.addWidget(self.labelLINK, 1, 0, 1, 1)
+        self.lineEditTITLE = QtWidgets.QLineEdit(self.groupBoxCode)
+        self.lineEditTITLE.setObjectName("lineEditTITLE")
+        self.gridLayout.addWidget(self.lineEditTITLE, 3, 1, 1, 1)
+        self.labelNAME = QtWidgets.QLabel(self.groupBoxCode)
+        self.labelNAME.setObjectName("labelNAME")
+        self.gridLayout.addWidget(self.labelNAME, 4, 0, 1, 1)
         self.labelInfo = QtWidgets.QLabel(self.groupBoxCode)
         self.labelInfo.setText("")
         self.labelInfo.setObjectName("labelInfo")
@@ -83,8 +92,9 @@ class Ui_dialog(object):
         self.verticalLayout_2.addLayout(self.verticalLayout)
 
         self.retranslateUi(dialog)
-        self.pushButtonEde.clicked.connect(dialog.close)
-        self.pushButtonSave.clicked.connect(self.clickSave)
+        self.pushButtonExit.clicked.connect(dialog.close)
+        self.pushButtonNext.clicked.connect(self.clickNext)
+        self.pushButtonOpen.clicked.connect(self.clickOffnen)
         self.radiobuttons = [
             self.radioButton1, self.radioButton2, self.radioButton3, self.radioButton4
         ]
@@ -95,33 +105,37 @@ class Ui_dialog(object):
         dialog.setTabOrder(self.lineEditBILD, self.lineEditTITLE)
         dialog.setTabOrder(self.lineEditTITLE, self.lineEditNAME)
         dialog.setTabOrder(self.lineEditNAME, self.radioButton1)
-        dialog.setTabOrder(self.radioButton1, self.pushButtonSave)
-        dialog.setTabOrder(self.pushButtonSave, self.pushButtonEde)
-        dialog.setTabOrder(self.pushButtonEde, self.radioButton2)
+        dialog.setTabOrder(self.radioButton1, self.pushButtonNext)
+        dialog.setTabOrder(self.pushButtonNext, self.pushButtonExit)
+        dialog.setTabOrder(self.pushButtonExit, self.radioButton2)
         dialog.setTabOrder(self.radioButton2, self.radioButton3)
         dialog.setTabOrder(self.radioButton3, self.radioButton4)
 
     def retranslateUi(self, dialog):
         _translate = QtCore.QCoreApplication.translate
-        dialog.setWindowTitle(_translate("dialog", "CodeCreator  v2.0.0"))
+        dialog.setWindowTitle(_translate("dialog", "CodeCreator  v2.0.5"))
         self.radioButton1.setToolTip(_translate("dialog", "Standard-Seite1"))
         self.radioButton1.setText(_translate("dialog", "Seite 1"))
         self.radioButton2.setText(_translate("dialog", "Seite 2"))
         self.radioButton3.setText(_translate("dialog", "Seite 3"))
         self.radioButton4.setText(_translate("dialog", "Seite 4"))
         self.groupBoxCode.setTitle(_translate("dialog", "Code für: Seite 1"))
-        self.labelLINK.setText(_translate("dialog", "LINK"))
-        self.labelTITLE.setText(_translate("dialog", "TITLE"))
-        self.labelNAME.setText(_translate("dialog", "NAME"))
-        self.lineEditNAME.setPlaceholderText(_translate("dialog", "Name"))
+        self.pushButtonNext.setToolTip(_translate("dialog", "Nächste Code"))
+        self.pushButtonNext.setText(_translate("dialog", "Next"))
+        self.pushButtonOpen.setToolTip(_translate("dialog", "Öffne fertige Code Datei"))
+        self.pushButtonOpen.setText(_translate("dialog", "Öffnen"))
+        self.pushButtonExit.setToolTip(_translate("dialog", "Beende CodeCreator"))
+        self.pushButtonExit.setText(_translate("dialog", "Beenden"))
         self.lineEditBILD.setPlaceholderText(_translate("dialog", "https://BILD.jpg"))
-        self.lineEditLINK.setPlaceholderText(_translate("dialog", "https://LINK.com"))
+        self.labelTITLE.setText(_translate("dialog", "TITLE"))
         self.labelBILD.setText(_translate("dialog", "BILD"))
+        self.lineEditNAME.setPlaceholderText(_translate("dialog", "Name"))
+        self.lineEditLINK.setPlaceholderText(_translate("dialog", "https://LINK.com"))
+        self.labelLINK.setText(_translate("dialog", "LINK"))
         self.lineEditTITLE.setPlaceholderText(_translate("dialog", "Titel"))
-        self.pushButtonSave.setText(_translate("dialog", "Speichern"))
-        self.pushButtonEde.setText(_translate("dialog", "Beenden"))
+        self.labelNAME.setText(_translate("dialog", "NAME"))
 
-    def clickSave(self):
+    def clickNext(self):
         output = f'<li><a href="{self.lineEditLINK.text()}" target="_blank"><img data-src="{self.lineEditBILD.text()}" class="lazyload" loading="lazy" title="{self.lineEditTITLE.text()}" border="2"/>{self.lineEditNAME.text()}</a></li>\n '
         with open(OUTPUT_FILENAME, "a", encoding='UTF-8') as output_file:
             output_file.write(output)
@@ -132,6 +146,11 @@ class Ui_dialog(object):
         self.lineEditBILD.clear()
         self.lineEditTITLE.clear()
         self.lineEditNAME.clear()
+
+    def clickOffnen(self):
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, OUTPUT_FILENAME])
+        #os.system(OUTPUT_FILENAME) #Für Windows
 
     def checkRadioButtonState(self):
         for radiobutton in self.radiobuttons:
